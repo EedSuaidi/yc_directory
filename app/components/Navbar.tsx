6a40ad0af6ae782f873f786cc5772b0ad8c69363
Moh. Su'aidi <moheedsuaidi@gmail.com>
2025-02-04 19:18:35 +0700

    Finish Authentication with NextAuth

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -10,7 +10,7 @@ const Navbar = async () => {
     <header className="font-work-sans bg-white px-5 py-3 shadow-sm">
       <nav className="flex items-center justify-between">
         <Link href="/">
-          <Image src="/logo.png" alt="Logo" width={144} height={30} />
+          <Image src="/logo.png" alt="logo" width={144} height={30} />
         </Link>
 
         <div className="flex items-center gap-5 text-black">
@@ -20,7 +20,15 @@ const Navbar = async () => {
                 <span>Create</span>
               </Link>
 
-              <button onClick={signOut}>Logout</button>
+              <form
+                action={async () => {
+                  'use server';
+
+                  await signOut({ redirectTo: '/' });
+                }}
+              >
+                <button type="submit">Logout</button>
+              </form>
 
               <Link href={`/user/${session?.id}`}>
                 <span>{session?.user?.name}</span>
