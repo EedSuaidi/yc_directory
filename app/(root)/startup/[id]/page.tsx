0c8996cbd3e64ab242e616817f5b014ddabdb841
Moh. Su'aidi <moheedsuaidi@gmail.com>
2025-02-07 16:19:51 +0700

    Finish Startup Details

--- app/(root)/startup/[id]/page.tsx
+++ app/(root)/startup/[id]/page.tsx
@@ -1,6 +1,16 @@
+import { Suspense } from 'react';
+import { formatDate } from '@/lib/utils';
 import { client } from '@/sanity/lib/client';
 import { STARTUP_BY_ID_QUERY } from '@/sanity/lib/queries';
 import { notFound } from 'next/navigation';
+import Image from 'next/image';
+import Link from 'next/link';
+
+import markdonwit from 'markdown-it';
+import { Skeleton } from '@/components/ui/skeleton';
+import View from '@/components/View';
+
+const md = markdonwit();
 
 export const experimental_ppr = true;
 
@@ -11,9 +21,69 @@ const page = async ({ params }: { params: Promise<{ id: string }> }) => {
 
   if (!post) return notFound();
 
+  const parsedContent = md.render(post?.pitch || '');
+
   return (
     <>
-      <h1 className="text-3xl">{post.title}</h1>
+      <section className="pink_container !min-h-[230px]">
+        <p className="tag">{formatDate(post?._createdAt)}</p>
+        <h1 className="heading">{post.title}</h1>
+        <p className="sub-heading !max-w-5xl">{post.description}</p>
+      </section>
+
+      <section className="section_container">
+        <Image
+          src={post.image}
+          alt="thumbnail"
+          width={1000}
+          height={1000}
+          className="h-auto w-full rounded-xl"
+        />
+
+        <div className="mx-auto mt-10 max-w-4xl space-y-5">
+          <div className="flex-between gap-5">
+            <Link
+              href={`/user/${post.author?._id}`}
+              className="mb-3 flex items-center gap-2"
+            >
+              <Image
+                src={post.author.image}
+                alt="avatar"
+                width={64}
+                height={64}
+                className="rounded-full drop-shadow-lg"
+              />
+
+              <div>
+                <p className="text-20-medium">{post.author.name}</p>
+                <p className="text-16-medium !text-black-300">
+                  @{post.author.username}
+                </p>
+              </div>
+            </Link>
+
+            <p className="category-tag">{post.category}</p>
+          </div>
+
+          <h3 className="text-30-bold">Pitch Details</h3>
+          {parsedContent ? (
+            <article
+              className="prose max-w-4xl break-all font-work-sans"
+              dangerouslySetInnerHTML={{ __html: parsedContent }}
+            />
+          ) : (
+            <p className="no-result">No details provided</p>
+          )}
+        </div>
+
+        <hr className="divider" />
+
+        {/* EDITOR SELECTED STARTUPS */}
+      </section>
+
+      <Suspense fallback={<Skeleton className="view_skeleton" />}>
+        <View id={id} />
+      </Suspense>
     </>
   );
 };
